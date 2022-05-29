cf0ecc2c778cb889b25d552fcfc8a77d848117b1
Yauheni Kapachenia <kapachenia@gmail.com>
2022-05-30 02:57:49 +0300

    Added favicon, implemented winder, improved adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 import {burger} from "./scripts/burger";
 import {autoHeightCatalog} from "./scripts/autoHeightCatalog";
+import {modal} from "./scripts/modal";
 
 const burgerSelector = document.querySelector('.burger');
-const menuSelector = document.querySelector('.nav__burger')
+const menuSelector = document.querySelector('.nav__burger');
+// const modal = document.querySelector('.modal');
 
 burger(burgerSelector, menuSelector)
 autoHeightCatalog();
---
+++ app/js/scripts/modal.js
@@ -0,0 +1,19 @@
+// export const modal = () => {
+//   let modal = document.getElementById("modal__project");
+//   let btn = document.getElementById("myBtn");
+//   let span = document.getElementsByClassName("close")[0];
+//
+//   btn.onclick = function() {
+//     modal.style.display = "block";
+//   }
+//
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+//
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+// }
